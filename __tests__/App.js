9c662cc9eb5a33190f6507c005389ae788063f80
zeven <fangfan1997@126.com>
2017-12-10 20:30:31 +0800

    eslint __tests__

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,12 @@
 import 'react-native';
 import React from 'react';
+import renderer from 'react-test-renderer';
 import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
+  renderer.create(
+    <App />,
   );
 });
