5e4e05692b6d8bb2682471dadbf84c3819acec5e
zeven <fangfan1997@126.com>
2017-12-10 20:27:07 +0800

    eslint index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './src/App';
+
 window.platform = Platform.OS;
 
 AppRegistry.registerComponent('ReactNativeWebDvaAntdMobile', () => App);
